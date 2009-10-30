f77f45131da09d4517345c8937c613c4a857893b
raymond <raymond@localhost>
2009-10-30 19:54:12 +0000

    Imported from MediaWiki svn r58372.
    Original log message:
      Localisation updates for core and extension messages from translatewiki.net (2009-10-30 19:30 UTC)

---
+++ Wikilog.i18n.magic.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Magic words used by Wikilog extension.
+ * @addtogroup Extensions
+ */
+
+$magicWords = array();
+
+/**
+ * English.
+ * @author Juliano F. Ravasi
+ */
+$magicWords['en'] = array(
+	'wl-settings'	=> array( 0, 'wl-settings'  ),
+	'wl-publish'	=> array( 0, 'wl-publish'   ),
+	'wl-author'		=> array( 0, 'wl-author'    ),
+	'wl-tags'		=> array( 0, 'wl-tags'      ),
+	'wl-info'		=> array( 0, 'wl-info'      ),
+	'wlk-icon'		=> array( 0, 'icon'         ),
+	'wlk-logo'		=> array( 0, 'logo'         ),
+	'wlk-subtitle'	=> array( 0, 'subtitle'     ),
+	'wlk-summary'	=> array( 1, 'summary'      ),
+	'wlk-hidden'	=> array( 0, 'hidden'       ),
+);
+
+/**
+ * Portuguese.
+ * @author Juliano F. Ravasi
+ */
+$magicWords['pt'] = array(
+	'wl-settings'	=> array( 0, 'wl-configs',  'wl-settings'  ),
+	'wl-publish'	=> array( 0, 'wl-publicar', 'wl-publish'   ),
+	'wl-author'		=> array( 0, 'wl-autor',    'wl-author'    ),
+	'wl-tags'		=> array( 0,                'wl-tags'      ),
+	'wl-info'		=> array( 0,                'wl-info'      ),
+	'wlk-icon'		=> array( 0, 'ícone',       'icon'         ),
+	'wlk-logo'		=> array( 0,                'logo'         ),
+	'wlk-subtitle'	=> array( 0, 'subtítulo',   'subtitle'     ),
+	'wlk-summary'	=> array( 1, 'resumo',      'summary'      ),
+	'wlk-hidden'	=> array( 0, 'oculto',      'hidden'       ),
+);
+
+/**
+ * Brazilian Portuguese.
+ * @author Juliano F. Ravasi
+ */
+$magicWords['pt-br'] = array(
+	'wl-settings'	=> array( 0, 'wl-configs',  'wl-settings'  ),
+	'wl-publish'	=> array( 0, 'wl-publicar', 'wl-publish'   ),
+	'wl-author'		=> array( 0, 'wl-autor',    'wl-author'    ),
+	'wl-tags'		=> array( 0,                'wl-tags'      ),
+	'wl-info'		=> array( 0,                'wl-info'      ),
+	'wlk-icon'		=> array( 0, 'ícone',       'icon'         ),
+	'wlk-logo'		=> array( 0,                'logo'         ),
+	'wlk-subtitle'	=> array( 0, 'subtítulo',   'subtitle'     ),
+	'wlk-summary'	=> array( 1, 'resumo',      'summary'      ),
+	'wlk-hidden'	=> array( 0, 'oculto',      'hidden'       ),
+);
---
+++ WlFeed.i18n.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Internationalisation file for extension Wikilog.
+ *
+ * @addtogroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Juliano F. Ravasi
+ */
+$messages['en'] = array(
+	# Extension description
+	'wlfeed-desc' => 'Enhanced feed generation classes',
+);
+
+/** Message documentation (Message documentation)
+ * @author Siebrand
+ */
+$messages['qqq'] = array(
+	'wlfeed-desc' => '{{desc}}',
+);
+
+/** French (Français)
+ * @author PieRRoMaN
+ */
+$messages['fr'] = array(
+	'wlfeed-desc' => 'Classes de génération de flux améliorés',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'wlfeed-desc' => 'Polěpšene klasy kanaloweje generacije',
+);
+
+/** Hungarian (Magyar)
+ * @author Glanthor Reviol
+ */
+$messages['hu'] = array(
+	'wlfeed-desc' => 'Fejlett hírcsatorna-generáló osztályok',
+);
+
+/** Japanese (日本語)
+ * @author Fryed-peach
+ */
+$messages['ja'] = array(
+	'wlfeed-desc' => '強化されたフィード生成クラス',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'wlfeed-desc' => 'Klassen voor het aanmaken van verbeterde feeds',
+);
+
+/** Piedmontese (Piemontèis)
+ * @author Borichèt
+ */
+$messages['pms'] = array(
+	'wlfeed-desc' => 'Class ëd generassion dij fluss ameliorà',
+);
+
+/** Portuguese (Português)
+ * @author Juliano F. Ravasi
+ */
+$messages['pt'] = array(
+	'wlfeed-desc' => 'Classes aprimoradas para geração de feeds',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'wlfeed-desc' => 'Расширенные классы подготовки лент новостей',
+);
+
